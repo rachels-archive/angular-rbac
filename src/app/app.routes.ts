a5055b8dacaec1e568b969e4c4fec4ddd6def0b3
rachels-archive <rachelhui@protonmail.com>
2024-04-19 12:20:43 +0800

    Create components, services and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'resgister', component: RegisterComponent },
+  { path: 'users', component: UserListComponent },
+];
