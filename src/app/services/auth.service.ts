4852eefe5bc2820a7fd2c20788f0aac5d1f7165c
rachels-archive <rachelhui@protonmail.com>
2024-04-19 17:00:19 +0800

    Add CRUD operations for auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  apiUrl = 'http://localhost:3000/users';
+
+  getAllUsers() {
+    return this.http.get(this.apiUrl);
+  }
+
+  getUserById(id: Number) {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  createUser(userData: any) {
+    return this.http.post(this.apiUrl, userData);
+  }
+
+  updateUser(id: any, userData: any) {
+    return this.http.put(`${this.apiUrl}/${id}`, userData);
+  }
+
+  deleteUser() {}
 }
