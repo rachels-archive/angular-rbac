e3333933036bc406f403fe89e646dbb20e2de620
rachels-archive <rachelhui@protonmail.com>
2024-04-20 09:03:15 +0800

    Create form for register component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    ReactiveFormsModule,
-    HttpClientModule,
-    MaterialModule,
-  ],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,31 +1,51 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
   constructor(
     private formBuilder: FormBuilder,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   registrationForm = this.formBuilder.group({
     id: ['', [Validators.required, Validators.minLength(5)]],
     name: ['', Validators.required],
-    password: ['', [Validators.required, Validators.minLength(5)]],
     email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(5)]],
     role: ['', Validators.required],
     isActive: [false],
   });
 
   confirmRegistration() {
     if (this.registrationForm) {
+      this.authService
+        .createUser(this.registrationForm.value)
+        .subscribe((res) => {
+          this.snackBar.open('User registered successfully');
+          this.router.navigate(['login']);
+        });
     } else {
       this.snackBar.open('Please enter valid inputs');
     }
